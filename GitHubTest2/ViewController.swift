82f81a16ca5b10e1627110e7eab5ea3d3289a20d
Siva Prakash <bvsivaprakash777@gmail.com>
2018-07-21 13:55:11 +0530

    1Commit

--- GitHubTest2/ViewController.swift
+++ GitHubTest2/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Abc1:49m 21/07/18")
     }
 
     override func didReceiveMemoryWarning() {
